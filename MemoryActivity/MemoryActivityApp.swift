3235c864fa9e6b4bcd018ca3adced3e93a765a4b
oshio <skw398@gmail.com>
2025-04-19 19:13:58 +0900

    init

---
+++ MemoryActivity/MemoryActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryActivityApp.swift
+//  MemoryActivity
+//
+//  Created by Shigenari Oshio on 2025/04/19.
+//
+
+import SwiftUI
+
+@main
+struct MemoryActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
