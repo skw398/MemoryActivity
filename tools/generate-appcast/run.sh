25e5738f9b51794ffe552026515b705c3b109073
oshio <skw398@gmail.com>
2025-08-23 14:23:21 +0900

    Copy artifacts in build phase

--- tools/generate-appcast/run.sh
+++ tools/generate-appcast/run.sh
@@ -2,4 +2,4 @@
 
 cp "$1" ./tools/generate_appcast/MemoryActivity.dmg
 cp ./project/Sparkle/appcast.xml ./tools/generate_appcast
-./DerivedData/MemoryActivity/SourcePackages/artifacts/sparkle/Sparkle/bin/generate_appcast ./tools/generate_appcast
+./artifacts/sparkle/Sparkle/bin/generate_appcast ./tools/generate_appcast
