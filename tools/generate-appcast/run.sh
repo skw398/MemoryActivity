255ad615f16f797b4b93f3bb4786d02197646582
oshio <skw398@gmail.com>
2026-03-28 00:07:15 +0900

    Remove obsolete distribution helper scripts

--- tools/generate-appcast/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cp "$1" ./tools/generate-appcast/MemoryActivity.dmg
-cp ./project/Sparkle/appcast.xml ./tools/generate-appcast
-./artifacts/sparkle/Sparkle/bin/generate_appcast ./tools/generate-appcast
